d13537e8c78c69bce7c74480cb2e3ef36cb41766
JorgeL91 <jorgeguti.0791@gmail.com>
2022-05-02 17:53:20 -0700

    reservation material and activities

--- src/components/reservation/ActivityResevation.js
+++ src/components/reservation/ActivityResevation.js
@@ -1,12 +1,61 @@
-import React from "react";
+import { Column } from "primereact/column";
+import { DataTable } from "primereact/datatable";
+import { InputText } from "primereact/inputtext";
+import React, { useState, useEffect } from "react";
+import { getActivities } from "../../service/general/activitiesServices";
 
 const ActivityResevation = () => {
+  const [activities, setActivities] = useState(null);
+  const [globalFilter, setGlobalFilter] = useState(null);
+  const [selectedActivities, setSelecteActivities] = useState(null);
+
+  const [show, setShow] = useState({
+    active: false,
+    severity: "error",
+    message: "",
+  });
+
+  useEffect(() => {
+    getMaterialData();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const getMaterialData = async () => {
+    const res = await getActivities();
+    if (!res.error) setActivities(res);
+  };
+
+  const header = (
+    <div className="flex flex-column md:flex-row md:justify-content-between md:align-items-center">
+      <span className="block mt-2 md:mt-0 p-input-icon-left">
+        <i className="pi pi-search" />
+        <InputText
+          type="search"
+          onInput={(e) => setGlobalFilter(e.target.value)}
+          placeholder="Buscar..."
+        />
+      </span>
+    </div>
+  );
+
   return (
-    <div className="flex align-items-center py-5 px-3">
-      <i className="pi pi-fw pi-user mr-2 text-2xl" />
-      <p className="m-0 text-lg">
-        ActivityResevation Component Content via Child Route
-      </p>
+    <div className="mt-5">
+      <DataTable
+        value={activities}
+        selection={selectedActivities}
+        onSelectionChange={(e) => setSelecteActivities(e.value)}
+        dataKey="idActividad"
+        responsiveLayout="scroll"
+        header={header}
+        globalFilter={globalFilter}
+      >
+        <Column
+          selectionMode="multiple"
+          headerStyle={{ width: "3em" }}
+        ></Column>
+        <Column field="nombre" header="Nombre"></Column>
+        <Column field="duracion" header="Duracion"></Column>
+        <Column field="observaciones" header="Observaciones"></Column>
+      </DataTable>
     </div>
   );
 };
--- src/components/reservation/MaterialResevation.js
+++ src/components/reservation/MaterialResevation.js
@@ -1,12 +1,122 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import InputCustom from "../formcustom/InputCustom";
+import { getMaterials } from "../../service/general/materialsServices";
+import { InputText } from "primereact/inputtext";
+import MsjToast from "../confirmation/MsjToast";
 
 const MaterialResevation = () => {
+  const [materials, setMaterials] = useState(null);
+  const [globalFilter, setGlobalFilter] = useState(null);
+  const [show, setShow] = useState({
+    active: false,
+    severity: "error",
+    message: "",
+  });
+
+  useEffect(() => {
+    getMaterialData();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  const getMaterialData = async () => {
+    const res = await getMaterials();
+    res.forEach((element) => {
+      element.count = 0;
+    });
+    if (!res.error) setMaterials(res);
+  };
+
+  const columns = [
+    { field: "nombre", header: "Nombre" },
+    { field: "stock", header: "Stock" },
+    { field: "count", header: "Cantidad" },
+    { field: "maximoPorDia", header: "Maximo por dia" },
+    { field: "observaciones", header: "Observaciones" },
+  ];
+  const cellEditor = (options) => {
+    //return textEditor(options);
+    if (options.field !== "count") {
+      return options.value;
+    } else {
+      return textEditor(options);
+    }
+  };
+
+  const textEditor = (options) => {
+    return (
+      <InputText
+        className="w-100"
+        type="text"
+        value={options.value}
+        onChange={(e) => options.editorCallback(e.target.value)}
+      />
+    );
+  };
+
+  const onCellEditComplete = (e) => {
+    let { rowData, newValue, field, originalEvent: event } = e;
+
+    if (newValue) {
+      if (newValue > rowData.stock) {
+        setShow({
+          ...show,
+          active: true,
+          message: "El valor no puede ser mayor al stock",
+        });
+      } else if (newValue > rowData.maximoPorDia) {
+        setShow({
+          ...show,
+          active: true,
+          message: "El valor no puede ser mayot al maximo por dia",
+        });
+      } else {
+        rowData[field] = newValue;
+      }
+    } else {
+      event.preventDefault();
+    }
+  };
+
+  const header = (
+    <div className="flex flex-column md:flex-row md:justify-content-between md:align-items-center">
+      <span className="block mt-2 md:mt-0 p-input-icon-left">
+        <i className="pi pi-search" />
+        <InputText
+          type="search"
+          onInput={(e) => setGlobalFilter(e.target.value)}
+          placeholder="Buscar..."
+        />
+      </span>
+    </div>
+  );
+
   return (
-    <div className="flex align-items-center py-5 px-3">
-      <i className="pi pi-fw pi-user mr-2 text-2xl" />
-      <p className="m-0 text-lg">
-        MaterialResevation Component Content via Child Route
-      </p>
+    <div className="mt-5">
+      <MsjToast show={show} setShow={setShow} />
+
+      <DataTable
+        value={materials}
+        editMode="cell"
+        className="editable-cells-table"
+        responsiveLayout="scroll"
+        header={header}
+        globalFilter={globalFilter}
+      >
+        {columns.map(({ field, header }) => {
+          return (
+            <Column
+              key={field}
+              field={field}
+              header={header}
+              style={{ width: "25%" }}
+              // body={field === "price" && priceBodyTemplate}
+              editor={(options) => cellEditor(options)}
+              onCellEditComplete={onCellEditComplete}
+            />
+          );
+        })}
+      </DataTable>
     </div>
   );
 };
